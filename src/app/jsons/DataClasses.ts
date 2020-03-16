765f3a7a93677802b94bb20d8719f577173ef181
Manish Aneja <manishekaneja@gmail.com>
2020-03-17 00:23:48 +0530

    WIP:partially updated

--- src/app/jsons/DataClasses.ts
+++ src/app/jsons/DataClasses.ts
@@ -1,6 +1,33 @@
+import { Validators } from '@angular/forms';
+
 export class Message {
   constructor(public message: string, public fav: boolean) { }
 }
-export class DataBlock{
-  constructor(public email: string, public password: string,public fullName?:string, public username?: string, public cpassword?: string ,public messages?:Message[],public tokenID?:string) { }
+export class DataBlock {
+  constructor(private _email?: string, private _password?: string, private _fullname?: string, private _messages?: Message[], private _tokenID?: string) { }
+  get email(): string {
+    return this._email || '';
+  }
+  get password(): string {
+    return this._password;
+  }
+  get fullname(): string {
+    return this._fullname;
+  }
+  get messages(): Array<Message> {
+    return this._messages;
+  }
+  public messagesCount(): number {
+    return this._messages.length;
+  }
+  public emailValid() {
+    return Validators.email(this._email)
+  }
+  set email(email: string) {
+    this._email = email;
+  }
+  set password(password: string) {
+    this._password = password;
+  }
+
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,39 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { DataBlock } from '../jsons/DataClasses';
 import { AjaxCallService } from '../ajax-call.service';
 import { Router } from '@angular/router';
 
 @Component({
-
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css', '../jsons/validation.css']
 })
-export class LoginComponent implements OnInit {
-
-  data = new DataBlock("", "");
+export class LoginComponent {
+  data = new DataBlock();
   invalidAttempt = false;
-  constructor(public ajaxCall: AjaxCallService, private router: Router) {
+  constructor(public ajaxCall: AjaxCallService, private router: Router) { }
+
+  public reset() {
+    delete this.data;
+    this.data = new DataBlock();
   }
-  ngOnInit() { }
-  reset() {
-    this.data.email = "";
-    this.data.password = "";
-  }
-  doLogin(): void {
-    this.ajaxCall.doLogin(this.data).subscribe((res) => {
-      let response: any = res;
-      if (response.valid) {
-        localStorage.tokenID = response.token;
-        this.ajaxCall.loggedInUser = true;
-        this.ajaxCall.preCheck().add(() => {
-          this.router.navigate(['/account/dashboard']);
-        });
-      }
-      else {
-        this.reset();
-        this.invalidAttempt = true;
-      }
-    })
+
+  public doLogin(): void {
+    console.log(this.data);
+    //   this.ajaxCall.doLogin(this.data).subscribe(res => {
+    //     let response: any = res;
+    //     if (response.valid) {
+    //       localStorage.tokenID = response.token;
+    //       this.ajaxCall.loggedInUser = true;
+    //       this.ajaxCall.preCheck().add(() => {
+    //         this.router.navigate(['/account/dashboard']);
+    //       });
+    //     }
+    //     else {
+    //       this.reset();
+    //       this.invalidAttempt = true;
+    //     }
+    //   })
   }
 }
